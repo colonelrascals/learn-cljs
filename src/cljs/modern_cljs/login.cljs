601030df07741ef47859b7449818dd8d5c5742f8
patrick.hildreth <patrick.hildreth@patrick-hildreth-mbp.local>
2018-06-28 09:40:13 -0500

    yet another update to the sand box

--- src/cljs/modern_cljs/login.cljs
+++ src/cljs/modern_cljs/login.cljs
@@ -1,3 +1,20 @@
 (ns modern-cljs.login
-  (:require `[domina.core :refer [by-id value set-value!]]))
+  (:require [domina.core :refer [by-id value set-value!]]))
+
+(defn validate-form []
+  "Using Domina"
+  (if (and (> (count (value (by-id "email"))) 0)
+           (> (count (value (by-id "password"))) 0))
+    true
+    (do (js/alert "Yo, complete the form!")
+        false)))
+
+(defn init []
+  "No Domina"
+  (if (and js/document
+           (.getElementById js/document))
+    (let [login-form (.getElementById js/document "loginForm")]
+      (set! (.-onsubmit login-form) validate-form))))
+
+(set! (.-onload js/window) init
 
